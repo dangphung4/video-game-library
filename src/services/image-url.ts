be7a33fc19e2b2295e25e59e0180ecfad683ead9
dangphung4 <dangphung4@gmail.com>
2023-08-19 11:30:23 -0400

    added sorting games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
 
 const getCroppedImage = (url: string) => {
+    if(!url) return "";
+    
     const t = "media/"
     const index = url.indexOf(t) + t.length;
     return url.slice(0,index) + 'crop/600/400/' + url.slice(index);
