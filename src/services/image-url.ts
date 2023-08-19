f8d87f506f89857d042fbb52a6716b8f95cd242d
dangphung4 <dangphung4@gmail.com>
2023-08-19 11:32:58 -0400

    handle no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,9 @@
+import noImage from "../assets/no-image-placeholder-6f3882e0.webp"
+
 
 const getCroppedImage = (url: string) => {
-    if(!url) return "";
-    
+    if(!url) return noImage;
+
     const t = "media/"
     const index = url.indexOf(t) + t.length;
     return url.slice(0,index) + 'crop/600/400/' + url.slice(index);
